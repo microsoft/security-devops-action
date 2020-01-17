0735181ff0548d533980cd839acaffa8db6f44f9
David Knise <daknis@microsoft.com>
2020-01-17 13:46:58 -0800

    Iterate on typescript action.

--- action.js
+++ action.js
@@ -1,1 +1,5 @@
+// import * as core from '@actions/core';
+// import * as exec from '@actions/exec';
+// import * as gdn from 'gdn-toolkit';
 console.write("Guardian GitHub Action");
+//new gdn.GuardianAction(core, exec).analyze(); 
