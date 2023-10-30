bab444e4220ea31af116950ef0290bab538aef52
David Knise <daknis@microsoft.com>
2023-10-30 11:35:05 -0700

    Implement features for scanning and mapping

---
+++ src/msdo-options.ts
@@ -0,0 +1,13 @@
+import { CommandType, Features } from './msdo-helpers';
+
+export interface IMsdoFeatures {
+    
+}
+
+export interface IMsdoOptions {
+    commandNameString: string;
+    commandName: CommandType;
+
+    featureString: string;
+    
+}
--- src/msdo.ts
+++ src/msdo.ts
@@ -1,5 +1,4 @@
 import * as core from '@actions/core';
-import { CommandType } from './msdo-helpers';
 import { IMicrosoftSecurityDevOps } from './msdo-interface';
 import * as client from '@microsoft/security-devops-actions-toolkit/msdo-client';
 import * as common from '@microsoft/security-devops-actions-toolkit/msdo-common';
@@ -26,13 +25,11 @@ export class MicrosoftSecurityDevOps implements IMicrosoftSecurityDevOps {
         core.debug('MicrosoftSecurityDevOps.runMain - Running MSDO...');
 
         let args: string[] = ['run'];
-        let scanEnabled: boolean = false;
 
         let config: string = core.getInput('config');
         if (!common.isNullOrWhiteSpace(config)) {
             args.push('-c');
             args.push(config);
-            scanEnabled = true;
         }
 
         let policy: string = core.getInput('policy');
@@ -47,9 +44,6 @@ export class MicrosoftSecurityDevOps implements IMicrosoftSecurityDevOps {
         if (!common.isNullOrWhiteSpace(categoriesString)) {
             args.push('--categories');
             let categories = categoriesString.split(',');
-            if (!scanEnabled) {
-                scanEnabled = categories.length > 0
-            }
             for (let i = 0; i < categories.length; i++) {
                 let category = categories[i];
                 if (!common.isNullOrWhiteSpace(category)) {
@@ -62,9 +56,6 @@ export class MicrosoftSecurityDevOps implements IMicrosoftSecurityDevOps {
         if (!common.isNullOrWhiteSpace(languagesString)) {
             args.push('--languages');
             let languages = languagesString.split(',');
-            if (!scanEnabled) {
-                scanEnabled = languages.length > 0
-            }
             for (let i = 0; i < languages.length; i++) {
                 let language = languages[i];
                 if (!common.isNullOrWhiteSpace(language)) {
@@ -76,21 +67,6 @@ export class MicrosoftSecurityDevOps implements IMicrosoftSecurityDevOps {
         let toolsString: string = core.getInput('tools');
         if (!common.isNullOrWhiteSpace(toolsString)) {
             let tools = toolsString.split(',');
-            const containerMappingIndex = tools.indexOf('container-mapping');
-            if (containerMappingIndex > -1) {
-                if (!scanEnabled && tools.length == 1) {
-                    // Only container mapping is enabled
-                    console.log("Scanning not enabled. Skipping...");
-                    return;
-                } else {
-                    core.debug("Removing non-scanner container-mapping");
-                    tools.splice(containerMappingIndex, 1);
-                }
-            }
-        }
-
-        let tools = [];
-        if (tools.length > 0) {
             args.push('--tool');
             for (let i = 0; i < tools.length; i++) {
                 let tool = tools[i];
